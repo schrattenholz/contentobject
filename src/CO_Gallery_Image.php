babb6da6511e47c62b49a449be1b0d4ad0a60d67
schrattenholz <46528228+schrattenholz@users.noreply.github.com>
2022-03-16 16:18:02 +0100

    Update CO_Gallery_Image.php

--- src/CO_Gallery_Image.php
+++ src/CO_Gallery_Image.php
@@ -5,7 +5,7 @@ namespace Schrattenholz\ContentObject;
 
 use Silverstripe\ORM\DataObject;
 use Silverstripe\Assets\Image;
-
+use SilverStripe\CMS\Model\SiteTree;
 class CO_Gallery_Image extends DataObject{
 	private static $table_name="CO_Gallery_Image";
 	private static $db=[
@@ -15,7 +15,8 @@ class CO_Gallery_Image extends DataObject{
 	];
 	private static $has_one=[
 		"Image"=>Image::class,
-		"Gallery"=>CO_Gallery::class
+		"Gallery"=>CO_Gallery::class,
+		'DeepLink'=>SiteTree::class
 	];
 	public function onBeforeWrite(){
 		parent::onBeforeWrite();
@@ -30,6 +31,23 @@ class CO_Gallery_Image extends DataObject{
 		  $this->Image()->doPublish();
 		}
 	 }
+	 	public function PageDeepLinkHash(){
+		$page=DataList::create(SiteTree::class)->where('ID='.$this->PageLinkID)->First;
+		if($this->DeepLinkHash){
+			return '#'.$this->DeepLink()->URLSegment;
+		}else{
+			return false;
+		}
+	}
+	public function PageLinkURL(){
+		$page=DataList::create(SiteTree::class)->where('ID='.$this->PageLinkID)->First;
+		if($this->DeepLinkHash){
+			//$page=DataList::create('SiteTree')->where('ID='.$this->DeepLink()->ParentID)->First();
+			return $this->DeepLink()->Parent()->Link();
+		}else{
+			return $this->DeepLink()->Link();
+		}
+	}
 	public function getThumbnail(){
 		return $this->Image()->CMSThumbnail();
 	}

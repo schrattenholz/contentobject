fcbe0e73bc33f379f794936ad1b9e729b6359508
schrattenholz <46528228+schrattenholz@users.noreply.github.com>
2022-03-31 15:44:23 +0200

    up

--- src/CO_Gallery_Image.php
+++ src/CO_Gallery_Image.php
@@ -37,8 +37,10 @@ class CO_Gallery_Image extends DataObject{
 		if($this->Title==""){
 			$this->Title=$this->Image()->Title;
 		}
-		
 	}
+	private static $extensions = [
+        Versioned::class,
+    ];
 	public function getCMSFields(){
 		$fields=parent::getCMSFields();
 		$fields->addFieldToTab('Root.Main',new Textfield('Title','Titel'));
@@ -55,13 +57,13 @@ class CO_Gallery_Image extends DataObject{
 		$fields->removeFieldFromTab('Root.Main','GalleryID');
 		return $fields;	
 	}
-	public function onAfterWrite(){
+	/*public function onAfterWrite(){
 	   parent::onAfterWrite();
-		if (!$this->ID && $this->Image()->exists()){
+		if ($this->Image()->exists() && !$this->Image()->isPublished()){
 			//Injector::inst()->get(LoggerInterface::class)->error($this->owner->MenuTitle.'   BlogExtension.php BasicExtension_MainImage MainImage()->ID='.$this->owner->MainImage()->Filename);
 		  $this->Image()->doPublish();
 		}
-	 }
+	 }*/
 	 	public function PageDeepLinkHash(){
 		$page=DataList::create(SiteTree::class)->where('ID='.$this->PageLinkID)->First;
 		if($this->DeepLinkHash){
